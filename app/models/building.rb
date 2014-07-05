429a37db190a6b5e0732683a515e5e2dffeae5c9
Artemiy Solopov <art-solopov@yandex.ru>
2014-07-06 02:51:31 +0400

    Filled Building model; Fixed database config.

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,2 +1,15 @@
 class Building < ActiveRecord::Base
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :latitude, presence: true
+  validates :longtitude, presence: true
+
+  scope :within, lambda {|lat, lon|
+    where(%q{earth_distance(
+              ll_to_earth(?, ?),
+              ll_to_earth(buildings.latitude, buildings.longtitude)
+            ) <= 4000},
+          lat, lon)
+  }
+
 end
--- test/models/building_test.rb
+++ test/models/building_test.rb
@@ -7,6 +7,24 @@ class BuildingTest < ActiveSupport::TestCase
   test 'should save the valid model' do
     v = buildings(:valid)
     assert v.valid?
-    assert v.save?
+    assert v.save
   end
+
+  %w{name address latitude longtitude}.each do |field|
+    test "should not save the model with no #{field}" do
+      v = buildings("no_#{field}".to_sym)
+      assert v.invalid?
+      assert !v.save
+    end
+  end
+
+  test 'should calculate the distances' do
+    right = %w{moscow_kremlin war_museum}.map{|e| buildings(e.to_sym)}
+    wrong = %w{big_ben}.map{|e| buildings(e.to_sym)}
+    lat, lon = [55.75326, 37.61873]
+    blds = Building.within(lat, lon)
+    right.each{|e| assert(blds.include?(e), "point #{e} should be within 4000 m")}
+    wrong.each{|e| assert(! blds.include?(e), "point #{e} shouldn't be within 4000 m")}
+  end
+
 end
